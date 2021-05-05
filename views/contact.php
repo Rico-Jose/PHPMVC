d4f1474746427009ee54f88c77c70d2a9346a723
Rico-Jose <imbangjoserico@gmail.com>
2021-05-05 11:12:06 +0800

    handle and sanitise submitted data

--- views/contact.php
+++ views/contact.php
@@ -3,7 +3,7 @@
 <form action="" method="post">
   <div class="form-group">
     <label>Subject</label>
-    <input type="email" name="subject" class="form-control">
+    <input type="subject" name="subject" class="form-control">
   </div>
   <div class="form-group">
     <label>Email</label>

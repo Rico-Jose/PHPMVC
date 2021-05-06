7eef9e83f5156529e638c63b79c00408ecc8f62c
Rico-Jose <imbangjoserico@gmail.com>
2021-05-06 10:03:04 +0800

    create AuthController | register and login layout and routing | auth layout routing

--- public/index.php
+++ public/index.php
@@ -2,20 +2,19 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-/* 
-echo '<pre>';
-var_dump();
-echo '</pre>';
-exit;
- */
-
 use app\core\Application;
 use app\controllers\SiteController;
+use app\controllers\AuthController;
 
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->post('/contact', [SiteController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
